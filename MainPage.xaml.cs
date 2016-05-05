d62bfdd88f6236c0d96254beeaeaad3608d4f884
muasstepa <muasstepa7@gmail.com>
2016-05-05 21:23:06 +0800

    Added files via upload

---
+++ MainPage.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Core;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace UkurCepat
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(View.ViewVolume.MainPage));
+        }
+
+        private void button_Click_1(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(View.ViewLuas.MainPage));
+        }
+
+        //private void button2_Click(object sender, RoutedEventArgs e)
+        //{
+        //    this.Frame.Navigate(typeof(View.ViewLuas.Lingkaran));
+        //}
+
+        //private void button3_Click(object sender, RoutedEventArgs e)
+        //{
+        //    this.Frame.Navigate(typeof(View.ViewVolume.VolumeBalok));
+        //}
+    }
+    
+}
